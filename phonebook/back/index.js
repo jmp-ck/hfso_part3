4e259cdd86c8813bbaa4e55bcb4cf05dce428f29
jmp-ck <hello@citronkiwi.fr>
2023-02-21 04:05:24 +0100

    exercise 3.14

--- phonebook/back/index.js
+++ phonebook/back/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -37,19 +39,33 @@ app.get('/info', (request, response) => {
   response.send('<p>Phonebook has info for ' + persons.length + ' people</p><p>' + new Date(Date.now()).toUTCString() + '</p>')
 })
 
+// app.get('/api/persons', (request, response) => {
+//   response.json(persons)
+// })
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const person = persons.find(person => person.id === id)
+//   // console.log(person)
+//   if (person) {
+//     response.json(person)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  // console.log(person)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(person => {
+    if (person)
+      response.json(person)
+    else {
+      response.status(400).json({ error: 'person not found' })
+    }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -58,11 +74,37 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateRandomId = (max) => Math.floor(Math.random() * max)
+// const generateRandomId = (max) => Math.floor(Math.random() * max)
+// app.post('/api/persons', (request, response) => {
+//   // console.log('header', request.headers)
+//   const body = request.body
+//   // console.log(body)
+//   if (!body.name) {
+//     return response.status(400).json({
+//       error: 'name is missing'
+//     })
+//   }
+//   if (!body.number) {
+//     return response.status(400).json({
+//       error: 'number is missing'
+//     })
+//   }
+//   const nameExists = persons.find(person => person.name === body.name)
+//   if (nameExists) {
+//     return response.status(400).json({
+//       error: 'name must be unique'
+//     })
+//   }
+//   const person = {
+//     id: generateRandomId(10000),
+//     name: body.name,
+//     number: body.number
+//   }
+//   persons = persons.concat(person)
+//   response.json(person)
+// })
 app.post('/api/persons', (request, response) => {
-  // console.log('header', request.headers)
   const body = request.body
-  // console.log(body)
   if (!body.name) {
     return response.status(400).json({
       error: 'name is missing'
@@ -79,13 +121,13 @@ app.post('/api/persons', (request, response) => {
       error: 'name must be unique'
     })
   }
-  const person = {
-    id: generateRandomId(10000),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT || 3001
